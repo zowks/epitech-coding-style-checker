4ae52cf9dd7f50c881aa7cd91f59d0a2d1d7e5b3
Zowks-VI <cyffers.merlin@gmail.com>
2025-01-14 09:46:23 +0000

    feat: Adding report summary

--- src/functions/output.ts
+++ src/functions/output.ts
@@ -9,14 +9,29 @@ const colors = {
 } as const;
 
 export default (report: Report) => {
+    const summary = { FATAL: 0, MAJOR: 0, MINOR: 0, INFO: 0 };
+
     for (const [path, violations] of Object.entries(report)) {
         Bun.write(stdout, `${path}\n\n`.strong.underline);
         for (const { type, line, code, comment } of violations) {
-            Bun.write(stdout, `${` ⏺ ${type.padEnd(5)} ${code}:`.strong} ${comment}\n`[colors[type]]);
-            Bun.write(stdout, `   ${path}:${line}\n\n`.italic.grey);
+            summary[type]++;
+            Bun.write(
+                stdout,
+                `${` ⏺ ${type.padEnd(5)} ${code}:`.strong} ${comment}\n`[colors[type]] +
+                `   ${path}:${line}\n\n`.italic.grey
+            );
         }
         Bun.write(stdout, '\n');
     }
+
+    Object.entries(summary).map(([type, count], index) => {
+        if (index !== 0)
+            Bun.write(stdout, ' | '.grey.strong);
+
+        Bun.write(
+            stdout,
+            `${String(count).underline.strong} ${type}`[colors[type as keyof typeof colors]]
+        );
+    });
+    Bun.write(stdout, '\n');
 }
-// │
-// └─
